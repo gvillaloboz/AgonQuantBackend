394f861484f7aa140bfb4d9967d2a6c6991f9644
Gabriela Villalobos <gavizu@gmail.coms>
2020-12-20 10:34:24 -0600

    update insertRecord to include user timestamp

--- insertRecord.php
+++ insertRecord.php
@@ -2,11 +2,12 @@
 	include 'db_config.php';
 	$connection = @mysqli_connect($host,$user,$password,$db);
 
-	$userId = $_POST['a'];
-	$weekNumber = $_POST['b'];
-	$suggestedNumSteps = $_POST['c'];
-	$choice = $_POST['d'];
-	$weeklyGoal = $_POST['e'];
+	$userId = $_POST['userId'];
+	$weekNumber = $_POST['weekNumber'];
+	$suggestedNumSteps = $_POST['suggestedNumSteps'];
+	$choice = $_POST['choice'];
+	$weeklyGoal = $_POST['weeklyGoal'];
+    $timestamp = $_POST['timestamp'];
 
 	if(!$connection){
 		echo "Error: " .mysqli_connect_error();
@@ -14,19 +15,17 @@
 	}
 
 	else{
-		//echo 'Connected to MySQL';
-		$sql = "INSERT INTO `goal_setting_record` (`user_id`, `week_number`, `suggested_number_of_steps`, `choice`, `weekly_goal`, `weekly_cumulative`, `goal_achieved`) VALUES ($userId, $weekNumber, $suggestedNumSteps, $choice, $weeklyGoal, '0', '-1')";	
+        $sql = "INSERT INTO `goal_setting_record` (`user_id`, `week_number`, `suggested_number_of_steps`, `choice`, `weekly_goal`, `weekly_cumulative`, `goal_achieved`, `server_timestamp`) VALUES ($userId, $weekNumber, $suggestedNumSteps, $choice, $weeklyGoal, '0', '-1', $timestamp)";
 
 		$query = mysqli_query($connection, $sql);
-		//echo 'Succesfully added';
-		//echo $query;
 
-		if ( false===$query ) {
-		  printf("error: %s\n", mysqli_error($connection));
+		if ($query === false) {
+            printf("error: %s\n", mysqli_error($connection));
+            header("HTTP/1.1 500 Server Error");
+            exit;
 		}
 		else {
-		  echo 'done';
-		  //printf("New record has id %d.\n",mysqli_insert_id($connection));
+            header('HTTP/1.1 200 OK');
 		}
 	}
 		// Query
