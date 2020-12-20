5b3512fa24a01997d2b3cc4fa9a6109f6c7091e3
Gabriela Villalobos <gavizu@gmail.coms>
2020-12-20 12:24:56 -0600

    update insertDailyGoal to include user timestamp

--- insertDailyGoal.php
+++ insertDailyGoal.php
@@ -9,7 +9,7 @@
         }
 	
         if(!isset($_POST['userId']) || !isset($_POST['goal']) ||
-           !isset($_POST['week']) || !isset($_POST['day'])) {
+           !isset($_POST['week']) || !isset($_POST['day']) || !isset($_POST['timestamp'])) {
                 header("HTTP/1.1 500 Server Error");
                 exit;
         }
@@ -19,18 +19,21 @@
 		$goal = intval($_POST['goal']);
 		$week = intval($_POST['week']);
 		$day = intval($_POST['day']);
+        $timestamp = $_POST['timestamp'];
 	} catch(Exception $e) {
                 header("HTTP/1.1 500 Server Error");
                 exit;	
 	}
 
 	if(!$connection){
+        
                 header("HTTP/1.1 500 Server Error");
                 exit;	
 	} else {
-		$sql = 'INSERT INTO daily_goal_record(week, day, goal, user_id) VALUES('.$week.', '.$day.', '.$goal.', '.$userId.')';
+		$sql = 'INSERT INTO daily_goal_record(week, day, goal, user_id, timestamp) VALUES('.$week.', '.$day.', '.$goal.', '.$userId.', '.$timestamp.')';
 		$q = mysqli_query($connection, $sql);
 		if ($q===false) {
+            printf("error: %s\n", mysqli_error($connection));
                 	header("HTTP/1.1 500 Server Error");
                 	exit;	
 		} else {
